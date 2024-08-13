6408649f4a47b5dc65b8eda83a865c69a53ab92c
mehtab39 <mehtabgill122@gmail.com>
2024-08-13 16:08:23 +0530

    Add: executeFunction on the fly

--- lambda/functionRunner.js
+++ lambda/functionRunner.js
@@ -1,7 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+const functionDirectory = path.join(__dirname, '../functions');
+
+
 process.on('message', (data) => {
     const { event, context, functionPath } = data;
     try {
         const func = require(functionPath);
+
+        context.callstack = [functionPath]
+
+        context.executeFunction = (childFuncName, childEvent) => {
+
+            const childFuncPath = path.join(functionDirectory, `${childFuncName}.js`);
+
+            if (!fs.existsSync(childFuncPath)) {
+                throw new Error('Function not found')
+            }
+
+            if (context.callstack.includes(childFuncPath)){
+                throw new Error('Recursion is not supported (Yet)!')
+            }
+
+            context.callstack.push(childFuncPath);
+
+            const childFunc = require(childFuncPath);
+            return new Promise((res, rej) => {
+                return childFunc(childEvent, context, (err, result) => {
+                   return  err ? rej(err) : res(result)
+                });
+            })
+        }
 
         context.stdout = (message) => {process.send({ log: message })};
         context.stderr = (message) => {process.send({ log: message })};
